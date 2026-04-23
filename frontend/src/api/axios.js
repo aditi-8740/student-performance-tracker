0e568b36e32cf30123223689f9267b0819f824a3
Aditi Sharma <aditisharma3711.as@gmail.com>
2026-04-24 02:34:26 +0530

    chore: connect frontend to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL : "https://student-performance-tracker-nfx4.onrender.com/",
+    baseURL : "https://student-performance-tracker-nfx4.onrender.com/api/",
 });
 
 // attach token automatically
